bdfc2fd1b9b5da2bd047c978cb2b1a1e3d7194bc
Tech-goodat <felixkiprotich2000@gmail.com>
2022-09-14 12:21:41 -0700

    header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,1 @@
-#include "main.h"
-/**
- * _putchar included in the head foldeer.
- * return is always zero to proove code workability.
- */
-int _putchar(char c)
-{char character[]= "_putchar";
-	putchar("character\n");
-	return 0;
-}
+
