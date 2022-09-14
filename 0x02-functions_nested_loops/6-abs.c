32f46f54b3c4a9abad73cb834b131b42a68f5fa3
Tech-goodat <felixkiprotich2000@gmail.com>
2022-09-14 13:10:32 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  *_abs - computes absolute value
+ *   *
+ *    *@i: variable of integer type
+ *     *
+ *      * Return: 0 always
+ *       */
+
+int _abs(int i)
+{
+
+		if (i >= 0)
+				{
+							return (i);
+								}
+			else
+					{
+								return (i * -1);
+									}
+			return (0);
+}
