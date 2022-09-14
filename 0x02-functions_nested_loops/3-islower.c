6c2c1c001decef62e03671a96eb0b8167f0148b6
Tech-goodat <felixkiprotich2000@gmail.com>
2022-09-14 13:02:24 -0700

    hello world

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * _islower - checks for lowercase character
+ *   *
+ *    *@c: variabe type int
+ *     *
+ *      * Return: Always 0
+ *       */
+
+int _islower(int c)
+{
+		if (c >= 'a' && c <= 'z')
+				{
+							return (1);
+								}
+			else
+					{
+								return (0);
+									}
+			return (0);
+}
