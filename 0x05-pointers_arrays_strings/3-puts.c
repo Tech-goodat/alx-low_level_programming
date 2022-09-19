064b90826b5916e0976a2cf2f799fa95f863dcdf
Tech-goodat <felixkiprotich2000@gmail.com>
2022-09-19 11:40:57 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  *_puts - Prints a string followed by a new line
+ *   *
+ *    * @str:  String value
+ *     *
+ *      */
+
+void _puts(char *str)
+{      
+		char str[] = "";
+
+			char *str = str;
+
+				int count = 0;
+
+					while (*(str + count) != '\0')
+							{
+										count++;
+											
+												_putchar(str[count]);
+													}
+
+						_putchar('\n');
+}
