e8c545869fa76408e753df9389865b91b7d0b6ec
Tech-goodat <felixkiprotich2000@gmail.com>
2022-09-29 03:21:15 -0700

    added all

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  *_strlen_recursion - prints lenght of a string
+ *   *
+ *    *@s: string
+ *     *
+ *      *Return: strlen
+ *       *
+ *        */
+
+int _strlen_recursion(char *s)
+{
+
+		if (*s == '\0')
+				{
+							return (0);
+								}
+
+			else
+					{
+								return (1 +  _strlen_recursion(s + 1));
+									}
+
+}
